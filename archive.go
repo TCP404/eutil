e6000a1da3ef0e419d272320fe0d8b0ef6927815
Boii <i@tcp404.com>
2022-10-30 22:49:36 +0800

    Feat: Zip Fix: size_fmt

---
+++ archive.go
@@ -0,0 +1,124 @@
+package eutil
+
+import (
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+)
+
+func Zip(dst *os.File, src string) error {
+	// 通过 dst 来创建 zip.Write
+	zw := zip.NewWriter(dst)
+	defer func() {
+		// 检测一下是否成功关闭
+		if err := zw.Close(); err != nil {
+			fmt.Println("cannot close file: ", err.Error())
+		}
+	}()
+
+	// 下面来将文件写入 zw ，因为有可能会有很多个目录及文件，所以递归处理
+	return filepath.Walk(src, func(path string, fi os.FileInfo, errBack error) error {
+		if errBack != nil || path == src {
+			return errBack
+		}
+
+		// 获取：文件头信息
+		fh, err := zip.FileInfoHeader(fi)
+		if err != nil {
+			return err
+		}
+		name, _ := filepath.Rel(src, path)
+		fh.Name = name
+		// 判断：文件是不是文件夹
+		if fi.IsDir() {
+			fh.Name += string(filepath.Separator)
+		} else {
+			// 设置：zip的文件压缩算法
+			fh.Method = zip.Deflate
+		}
+
+		// 创建：压缩包头部信息
+		w, err := zw.CreateHeader(fh)
+		if err != nil {
+			return err
+		}
+
+		// 检测，如果不是标准文件就只写入头信息，不写入文件数据到 w
+		// 如目录，也没有数据需要写
+		if !fh.Mode().IsRegular() {
+			return nil
+		}
+
+		// 目录到这就可以结束了
+		if fi.IsDir() {
+			return nil
+		}
+		// 打开要压缩的文件
+		fr, err := os.Open(path)
+		defer fr.Close()
+		if err != nil {
+			return err
+		}
+
+		// 将打开的文件 Copy 到 w
+		_, err = io.Copy(w, fr)
+		return err
+	})
+}
+
+func UnZip(dst, src string) error {
+	// 打开压缩文件，这个 zip 包有个方便的 ReadCloser 类型
+	// 这个里面有个方便的 OpenReader 函数，可以比 tar 的时候省去一个打开文件的步骤
+	zr, err := zip.OpenReader(src)
+	defer zr.Close()
+	if err != nil {
+		return err
+	}
+
+	// 如果解压后不是放在当前目录就按照保存目录去创建目录
+	if dst != "" {
+		if err := os.MkdirAll(dst, 0755); err != nil {
+			return err
+		}
+	}
+
+	// 遍历 zr ，将文件写入到磁盘
+	for _, file := range zr.File {
+		path := filepath.Join(dst, file.Name)
+
+		// 如果是目录，就创建目录
+		if file.FileInfo().IsDir() {
+			if err := os.MkdirAll(path, file.Mode()); err != nil {
+				return err
+			}
+			// 因为是目录，跳过当前循环，因为后面都是文件的处理
+			continue
+		}
+
+		// 获取到 Reader
+		fr, err := file.Open()
+		if err != nil {
+			return err
+		}
+
+		// 创建要写出的文件对应的 Write
+		fw, err := os.OpenFile(path, os.O_CREATE|os.O_RDWR|os.O_TRUNC, file.Mode())
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(fw, fr)
+		if err != nil {
+			return err
+		}
+
+		// 因为是在循环中，无法使用 defer ，直接放在最后
+		// 不过这样也有问题，当出现 err 的时候就不会执行这个了，
+		// 可以把它单独放在一个函数中，这里是个实验，就这样了
+		fw.Close()
+		fr.Close()
+	}
+	return nil
+}
--- size_fmt.go
+++ size_fmt.go
@@ -3,7 +3,7 @@ package eutil
 import "strconv"
 
 const (
-	B int64 = 1 << (10*iota + 3)
+	B int64 = 1 << (10 * iota)
 	KB
 	MB
 	GB
@@ -14,36 +14,36 @@ const (
 // sizeFmt 格式化给定的文件大小，如果文件类型为目录，则返回 "-"
 //
 // 参数：
-//		bit int64: 文件大小的比特数
+//
+//	bit int64: 文件大小的比特数
+//
 // 返回值：
-// 		string: 格式化后的文件大小，单位 KByte、MByte、GByte、TByte、PByte
-func SizeFmt(bit int64) string {
-	sizeFloat := float64(bit)
+//
+//	string: 格式化后的文件大小，单位 KByte、MByte、GByte、TByte、PByte
+func SizeFmt(bSize int64) string {
+	sizeFloat := float64(bSize)
 	size := "-"
-	unit := "b"
-	if bit == 0 {
-		return size
-	}
+	unit := "B"
+
 	switch {
-	case bit < B:
-		return strconv.FormatInt(bit, 10) + unit
-	case bit >= B && bit < KB:
-		sizeFloat /= 1 << 3
-		unit = "B"
-	case bit >= KB && bit < MB:
-		sizeFloat /= 1 << 13
+	case bSize < B:
+		return size
+	case bSize >= B && bSize < KB:
+		return strconv.FormatInt(bSize, 10) + unit
+	case bSize >= KB && bSize < MB:
+		sizeFloat /= 1 << 10
 		unit = "KB"
-	case bit >= MB && bit < GB:
-		sizeFloat /= 1 << 23
+	case bSize >= MB && bSize < GB:
+		sizeFloat /= 1 << 20
 		unit = "MB"
-	case bit >= GB && bit < TB:
-		sizeFloat /= 1 << 33
+	case bSize >= GB && bSize < TB:
+		sizeFloat /= 1 << 30
 		unit = "GB"
-	case bit >= TB && bit < PB:
-		sizeFloat /= 1 << 43
+	case bSize >= TB && bSize < PB:
+		sizeFloat /= 1 << 40
 		unit = "TB"
-	case bit >= PB:
-		sizeFloat /= 1 << 53
+	case bSize >= PB:
+		sizeFloat /= 1 << 50
 		unit = "PB"
 	}
 	return strconv.FormatFloat(sizeFloat, 'f', 1, 64) + unit
